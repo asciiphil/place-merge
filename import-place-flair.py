f1154ebb9290af0978013313fed0d6f01edf2b6e
Phil! Gold <phil_g@pobox.com>
2017-04-17 09:16:12 -0400

    Put known placements (from /r/place flair) in working database.

--- import-place-flair.py
+++ import-place-flair.py
@@ -4,16 +4,22 @@
 pixel placements.  This gives known timestamps for particular
 placements."""
 
+import argparse
 import re
 import sys
 import sqlite3
 
 from bs4 import BeautifulSoup
 
-known_conn = sqlite3.connect('known.sqlite')
+parser = argparse.ArgumentParser()
+parser.add_argument('-w', '--working-database', default='working.sqlite', help='Intermediate SQLite database to use.  Default: %(default)s.')
+parser.add_argument('input', nargs='+', type=argparse.FileType('r'), help='HTML file with flair from /r/place.')
+args = parser.parse_args()
+
+known_conn = sqlite3.connect(args.working_database)
 known_cur = known_conn.cursor()
 known_cur.execute("""
-CREATE TABLE IF NOT EXISTS known (
+CREATE TABLE IF NOT EXISTS known_placements (
   timestamp REAL,
   x INTEGER,
   y INTEGER,
@@ -23,11 +29,12 @@ CREATE TABLE IF NOT EXISTS known (
 known_conn.commit()
 
 def add_placement(timestamp, x, y, color, user):
-    known_cur.execute('SELECT COUNT(*) FROM known WHERE timestamp = ? AND x = ? and y = ? and color = ? and author = ?', (timestamp, x, y, color, user))
+    known_cur.execute('SELECT COUNT(*) FROM known_placements WHERE timestamp = ? AND x = ? and y = ? and color = ? and author = ?', (timestamp, x, y, color, user))
     if known_cur.fetchone()[0] == 0:
-        known_cur.execute('INSERT INTO known (timestamp, x, y, color, author) VALUES (?, ?, ?, ?, ?)', (timestamp, x, y, color, user))
-    
-with open(sys.argv[1], 'r') as input_file:
+        known_cur.execute('INSERT INTO known_placements (timestamp, x, y, color, author) VALUES (?, ?, ?, ?, ?)', (timestamp, x, y, color, user))
+
+for input_file in args.input:
+    print 'Reading {}...'.format(input_file.name)
     soup = BeautifulSoup(input_file.read(), 'lxml')
     for span in soup.find_all(attrs={'class':'flair'}):
         # class is "flair flair-place-n", where "n" is the color ID
@@ -40,5 +47,4 @@ with open(sys.argv[1], 'r') as input_file:
         #
         user = span.previous_sibling.text
         add_placement(timestamp, x, y, color, user)
-        
-known_conn.commit()
+    known_conn.commit()
